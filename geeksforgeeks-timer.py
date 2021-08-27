b2cb6bca8af793ef944ee48a2bcef286ecf10ab9
Stanislav Zaleski <sigmaray@gmail.com>
2021-08-27 21:54:03 +0300

    .

--- geeksforgeeks-timer.py
+++ geeksforgeeks-timer.py
@@ -6,7 +6,68 @@ from PyQt5.QtCore import *
 import sys
 from time_ended_dialog import TimeEndedDialog
 
+def genText(seconds):
+    minInt = round(seconds / 60)
+    hFloat  = float(seconds) / 60 / 60
+    hInt = round(seconds / 60 / 60)
+    if seconds <= 99:
+        return str(seconds) + "s"
+    elif minInt < 10 :
+        return str(round(float(seconds) / 60, 1)) + "m"
+    elif minInt >= 10 and minInt < 60:
+        return str(minInt) + "m"
+    elif minInt >= 60 and hInt < 10:
+        return str(round(hFloat, 1)) + "h"
+    elif hInt >= 10:
+        return str(hInt) + "h"
+
 class Window(QMainWindow):
+
+	def addTrayIcon(self):
+		# Adding an icon
+		# icon = QIcon("icon.png")
+		icon = QIcon()
+
+
+		pixmap = QPixmap(24, 24)
+		pixmap.fill(QtCore.Qt.white)
+		painter = QPainter(pixmap)
+		# painter.drawText(pixmap.rect(), QtCore.Qt.AlignCenter, "Hi!")
+		# painter.drawText(pixmap.rect(), QtCore.Qt.AlignCenter, "hello")
+		s = 40
+		painter.setFont(QFont('Arial', 9))
+		painter.drawText(pixmap.rect(), QtCore.Qt.AlignCenter, genText(s))
+		painter.end()
+		icon = QIcon(pixmap);
+		self.tray = QSystemTrayIcon()
+
+		# Creating the options
+		menu = QMenu()
+		option1 = QAction("Geeks for Geeks")
+		option2 = QAction("GFG")
+		menu.addAction(option1)
+		menu.addAction(option2)
+
+		# # # To quit the app
+		q = QAction("Quit")
+		# quit.triggered.connect(self.quit)
+		q.triggered.connect(QApplication.quit)		
+		menu.addAction(q)
+
+		# Adding options to the System Tray
+		self.tray.setContextMenu(menu)
+
+		self.tray.setIcon(icon)
+		self.tray.setToolTip("Hi!")
+		self.tray.setVisible(True)
+
+		# Adding item on the menu bar
+		# tray = QSystemTrayIcon()
+		# tray.setIcon(icon)
+		# tray.setVisible(True)
+
+
+
 
 	def __init__(self):
 		super().__init__()
@@ -21,6 +82,8 @@ class Window(QMainWindow):
 
 		# calling method
 		self.UiComponents()
+
+		# self.addTrayIcon()
 
 		# showing all the widgets
 		self.show()
