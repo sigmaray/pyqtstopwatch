14c6bff2ad2fa792701e27ea1172480e767f59a9
Stanislav Zaleski <sigmaray@gmail.com>
2023-09-12 16:28:19 +0600

    .

---
+++ build-exe.bat
@@ -0,0 +1,2 @@
+python -m PyInstaller stopwatch.pyw --noconsole --paths=stopwatch --paths=include --add-data "stopwatch;stopwatch" --name stopwatch --noconfirm
+python -m PyInstaller timer.pyw --noconsole --paths=timer --paths=include --add-data "timer;timer" --name timer --noconfirm
